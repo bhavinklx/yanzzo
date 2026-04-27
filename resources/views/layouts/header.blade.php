0e2a6835bed5418e144417124575043d674b4092
Bhavin Kalena <bhavin@axinenterprise.com>
2026-04-28 02:49:41 +0530

    Email related change

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,13 +11,13 @@
                     </span>
                 </a>
                 <a href="{{ url('/') }}" class="navbar-brand logo">
-                    <img src="{{ url('/image/logo.png') }}" class="img-fluid" alt="Logo">
+                    <img src="{{ asset('image/logo.png') }}') }}" class="img-fluid" alt="Logo">
                 </a>
             </div>
             <div class="main-menu-wrapper">
                 <div class="menu-header">
                     <a href="{{ url('/') }}" class="menu-logo">
-                        <img src="{{ url('/image/logo.png') }}" class="img-fluid" alt="Logo">
+                        <img src="{{ asset('image/logo.png') }}') }}" class="img-fluid" alt="Logo">
                     </a>
                     <a id="menu_close" class="menu-close" href="javascript:void(0);"> <i class="fas fa-times"></i></a>
                 </div>
